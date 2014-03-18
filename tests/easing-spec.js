498ddaad71d37604fc6ba2d45bf156ede65e901a
afc163 <afc163@gmail.com>
2014-03-18 10:58:38 +0800

    fix test case in dist

--- tests/easing-spec.js
+++ tests/easing-spec.js
@@ -2,13 +2,12 @@ define(function(require) {
 
     var $ = require('$');
     var expect = require('expect');
+    require('easing');
 
     describe('easing', function() {
 
         it('add easing function', function() {
             expect($.easing.linear).to.be.ok();
-            expect($.easing.easeOutStrong).to.not.be.ok();
-            require('../src/easing');
             expect($.easing.easeOutStrong).to.be.ok();
         });
 
