5fe192949b8e7d4865c9d079512c042e9fa4bce5
chencheng <sorrycc@gmail.com>
2014-05-15 18:09:35 +0800

    1.1.0, upgrade to spm@3x

--- tests/easing-spec.js
+++ tests/easing-spec.js
@@ -1,16 +1,13 @@
-define(function(require) {
 
-    var $ = require('$');
-    var expect = require('expect');
-    require('easing');
+var $ = require('jquery');
+var expect = require('expect.js');
+require('../index.js');
 
-    describe('easing', function() {
+describe('easing', function() {
 
-        it('add easing function', function() {
-            expect($.easing.linear).to.be.ok();
-            expect($.easing.easeOutStrong).to.be.ok();
-        });
-
+    it('add easing function', function() {
+        expect($.easing.linear).to.be.ok();
+        expect($.easing.easeOutStrong).to.be.ok();
     });
 
 });
